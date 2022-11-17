ca56b441f59724f668fd868951df82a79f42aad1
Shawin <gongerwang@gmail.com>
2022-11-16 21:49:21 -0600

    fix the create note function for image

--- src/App.js
+++ src/App.js
@@ -76,63 +76,70 @@ const App = ({signOut}) => {
 
     return (
 
-        <><View className="App">
-        <Heading level={0}>My Notes App</Heading>
-        <View as="form" margin="2rem 0" onSubmit={createNote}>
-          <Flex direction="row" justifyContent="center">
-            <TextField
-              name="name"
-              placeholder="Note Name"
-              label="Note Name"
-              labelHidden="labelHidden"
-              variation="quiet"
-              required="required" />
-            <TextField
-              name="description"
-              placeholder="Note Description"
-              label="Note Description"
-              labelHidden="labelHidden"
-              variation="quiet"
-              required="required" />
-            <Button type="submit" variation="primary">
-              Create Note
-            </Button>
-          </Flex>
+        <View className="App">
+            
+            <Heading level={0}>My Notes App</Heading>
+            <View as="form" margin="2rem 0" onSubmit={createNote}>
+                <Flex direction="row" justifyContent="center">
+                    <TextField
+                        name="name"
+                        placeholder="Note Name"
+                        label="Note Name"
+                        labelHidden="labelHidden"
+                        variation="quiet"
+                        required="required"/>
+                    <TextField
+                        name="description"
+                        placeholder="Note Description"
+                        label="Note Description"
+                        labelHidden="labelHidden"
+                        variation="quiet"
+                        required="required"/>
+                    <View
+                        name="image"
+                        as="input"
+                        type="file"
+                        style={{ alignSelf: "end" }}/>
+                    <Button type="submit" variation="primary">
+                        Create Note
+                    </Button>
+                </Flex>
+            </View>
+
+            <Heading level={1}>Current Notes</Heading>
+            <View margin="2rem 0">
+                {
+                    notes.map((note) => (
+                        <Flex
+                            key={note.id || note.name}
+                            direction="row"
+                            justifyContent="center"
+                            alignItems="center">
+                            <Text as="strong" fontWeight={699}>
+                                {note.name}
+                            </Text>
+                            <Text as="span">{note.description}</Text>
+                            {
+                                note.image && (
+                                    <Image
+                                        src={note.image}
+                                        alt={`visual aid for ${notes.name}`}
+                                        style={{
+                                            width: 399
+                                        }}/>
+                                )
+                            }
+                            <Button variation="link" onClick={() => deleteNote(note)}>
+                                Delete note
+                            </Button>
+                        </Flex>
+                    ))
+                }
+            </View>
+
+            <Button onClick={signOut}>Sign Out</Button>
         </View>
 
-        <Heading level={1}>Current Notes</Heading>
-        <View margin="2rem 0">
-          {notes.map((note) => (
-            <Flex
-              key={note.id || note.name}
-              direction="row"
-              justifyContent="center"
-              alignItems="center">
-              <Text as="strong" fontWeight={699}>
-                {note.name}
-              </Text>
-              <Text as="span">{note.description}</Text>
-              {note.image && (
-                <Image
-                  src={note.image}
-                  alt={`visual aid for ${notes.name}`}
-                  style={{
-                    width: 399
-                  }} />
-              )}
-              <Button variation="link" onClick={() => deleteNote(note)}>
-                Delete note
-              </Button>
-            </Flex>
-          ))}
-        </View>
-
-        <Button onClick={signOut}>Sign Out</Button>
-      </View><View
-          name="image"
-          as="input"
-          type="file"
-          style={{ alignSelf: "end" }} /></>
     );
 };
 
